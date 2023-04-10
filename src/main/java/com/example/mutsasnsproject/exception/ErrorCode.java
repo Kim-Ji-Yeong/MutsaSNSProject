9b0b6123744dae40e3038d5c81c88bbb5f761b01
Kim-Ji-Yeong <yeong6415@daum.net>
2023-04-10 16:03:32 +0900

    exception

---
+++ src/main/java/com/example/mutsasnsproject/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.example.mutsasnsproject.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    USERNAME_DUPLICATED(HttpStatus.CONFLICT,"username conflict"),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,""),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,""),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+    ALREADY_LIKED(HttpStatus.CONFLICT,"이미 좋아요 누른상태")
+    ;
+    private HttpStatus httpStatus;
+    private String message;
+
+}
---
+++ src/main/java/com/example/mutsasnsproject/exception/ExceptionManager.java
@@ -0,0 +1,34 @@
+package com.example.mutsasnsproject.exception;
+
+import com.example.mutsasnsproject.domain.dto.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+
+
+//특정 예외처리를 받아 처리하게 할 수 있음
+@RestControllerAdvice
+public class  ExceptionManager {
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<?> appExceptionHandler(AppException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode(), e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                .body(Response.error("ERROR", errorResponse));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> SQLExceptionHandler(SQLException e){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.DATABASE_ERROR, e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error("ERROR", errorResponse));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+}
